d5d1f3edccda8afce5503f464b6d151ea7d7c2a1
Mr Charst <maiquyenlinh15320@gmail.com>
2021-12-28 23:09:00 +0700

    App_CoVid-19

---
+++ app/src/main/java/com/developer/arsltech/covid_19tracker/DetailActivity.java
@@ -0,0 +1,84 @@
+package com.developer.arsltech.covid_19tracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.leo.simplearcloader.SimpleArcLoader;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private  int positionCountry;
+
+    PieChart pieChart1;
+    TextView tvCountry,tvCases,tvRecovered,tvCritical,tvActive,tvTodayCases,tvTotalDeaths,tvTodayDeaths;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        positionCountry = intent.getIntExtra("position",0);
+
+        getSupportActionBar().setTitle("Details of "+AffectedCountries.countryModelsList.get(positionCountry).getCountry());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        pieChart1 = findViewById(R.id.piechart1);
+        tvCountry = findViewById(R.id.tvCountry);
+        tvCases = findViewById(R.id.tvCases);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvActive = findViewById(R.id.tvActive);
+        tvTodayCases = findViewById(R.id.tvTodayCases);
+        tvTotalDeaths = findViewById(R.id.tvDeaths);
+        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
+
+        tvCountry.setText(AffectedCountries.countryModelsList.get(positionCountry).getCountry());
+        tvCases.setText(AffectedCountries.countryModelsList.get(positionCountry).getCases());
+        tvRecovered.setText(AffectedCountries.countryModelsList.get(positionCountry).getRecovered());
+        tvCritical.setText(AffectedCountries.countryModelsList.get(positionCountry).getCritical());
+        tvActive.setText(AffectedCountries.countryModelsList.get(positionCountry).getActive());
+        tvTodayCases.setText(AffectedCountries.countryModelsList.get(positionCountry).getTodayCases());
+        tvTotalDeaths.setText(AffectedCountries.countryModelsList.get(positionCountry).getDeaths());
+        tvTodayDeaths.setText(AffectedCountries.countryModelsList.get(positionCountry).getTodayDeaths());
+
+
+
+
+        pieChart1.addPieSlice(new PieModel("Casess",Integer.parseInt(tvCases.getText().toString()), Color.parseColor("#FFA726")));
+        pieChart1.addPieSlice(new PieModel("Recoverds",Integer.parseInt(tvRecovered.getText().toString()), Color.parseColor("#66BB6A")));
+        pieChart1.addPieSlice(new PieModel("Deathss",Integer.parseInt(tvTotalDeaths.getText().toString()), Color.parseColor("#EF5350")));
+        pieChart1.addPieSlice(new PieModel("Actives",Integer.parseInt(tvActive.getText().toString()), Color.parseColor("#29B6F6")));
+        pieChart1.startAnimation();
+        
+
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+}
